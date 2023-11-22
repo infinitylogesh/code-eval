421496e2f2a9129318f0d6a77999d84f13c418be
Logesh Kumar Umapathi <lk@blackboxai.tech>
2023-11-22 04:21:21 +0000

    updated temeperature sweep script

--- eval.sh
+++ eval.sh
@@ -2,20 +2,22 @@
 
 
 
-model="/home/ubuntu/pangu_coder2/outputs/instruction_tuning/glaive_multitask_pyexcer_func_comp_cont_script_comp/merged_weights/"
-tokenizer_name="/home/ubuntu/pangu_coder2/outputs/instruction_tuning/glaive_multitask_pyexcer_func_comp_cont_script_comp/merged_weights/"
-output_folder="/home/ubuntu/code-eval/results/eval_glaive_multitask_pyexcer_func_comp_cont_script_comp/"
-output_file_name="eval_n_samples_10"
+model=${1:-"/home/ubuntu/pangu_coder2/outputs/instruction_tuning/glaive_multitask_pyexcer_func_comp_cont_script_comp/merged_weights/"}
+tokenizer_name=${2:-"/home/ubuntu/pangu_coder2/outputs/instruction_tuning/glaive_multitask_pyexcer_func_comp_cont_script_comp/merged_weights/"}
+output_folder=${3:-"/home/ubuntu/code-eval/results/eval_glaive_multitask_pyexcer_func_comp_cont_script_comp/"}
+temperature=${4:-0.2}
+n_samples=${5:-10}
+max_new_tokens=${6:-768}
+output_file_name="eval_n_samples_${n_samples}"
 output_file="${output_folder}/${output_file_name}_${temperature}.jsonl"
-temperature=0.2
 
 echo "Running Humaneval for Model:${model} , temperature:${temperature}"
-python3 eval_glaivecoder.py --model_name_or_path $model \
+python3 eval_completion.py --model_name_or_path $model \
                                 --tokenizer_name $tokenizer_name \
                                 --output_file $output_file \
                                 --temperature $temperature \
-                                --max_new_tokens 768 \
-                                --n_samples 10
+                                --max_new_tokens $max_new_tokens \
+                                --n_samples $n_samples
 echo "Executing results: ${output_file}"
 log_file="${output_folder}/log_${output_file_name}_${temperature}.txt"
 score_file="${output_folder}/results_${output_file_name}_${temperature}.txt"
--- temperature_sweep.sh
+++ temperature_sweep.sh
@@ -1,32 +1,34 @@
-model="/home/ubuntu/pangu_coder2/outputs/instruction_tuning/starcoder7b_codealpaca_v2/merged_weights/"
-model="glaiveai/glaive-coder-7B"
-tokenizer_name="glaiveai/glaive-coder-7B"
-output_folder="/home/ubuntu/code-eval/results/eval_glaive_instruct"
+#!/bin/bash
 
+set -e
+
+model="/workspace/RLAIF/outputs/dpo_deepseek_instruct_multitask_v2_pyexcer_func_script_comp_v2/merged_weights/"
+tokenizer_name="/workspace/RLAIF/outputs/dpo_deepseek_instruct_multitask_v2_pyexcer_func_script_comp_v2/merged_weights/"
+output_folder="/workspace/code-eval/results/eval_deepseek_instruct_multitask_v2_pyexcer_func_script_comp_v2/"
+n_samples=50
+max_new_tokens=768
 
 declare -a temperatures=(
                 0.0
                 0.1
                 0.2
                 0.2
-#                0.3
-#                0.4
-#                0.5
-#                0.6
-#                0.7
-#                0.8
-#                0.9
-#                1.0
-#                1.0
+                0.3
+                0.4
+                0.5
+                0.6
+                0.7
+                0.8
+                0.9
+                1.0
+                1.0
                 )
 
 for temperature in "${temperatures[@]}":
 do
+    if [[ "$temperature" = 0.0 ]]; then
+        n_samples=1
+    fi
     echo "Running Humaneval for temperature:${temperature}"
-    python3 eval_glaivecoder.py --model_name_or_path $model \
-                                --tokenizer_name $tokenizer_name \
-                                --output_file "${output_folder}/${output_file_name}_${temperature}.jsonl" \
-                                --temperature $temperature \
-                                --max_new_tokens 768 \
-                                --n_samples 10
+    bash eval.sh $model $tokenizer_name $output_folder $temperature $n_samples $max_new_tokens
 done
